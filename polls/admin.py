4b32b5b9e7951b7899b5a728e3d9dafc5e283da8
KamilTerelak1 <19dg.kamil.terelak@ezse.pl>
2021-10-08 14:03:40 +0200

    commituje

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+class ChoiceInline(admin.TabularInline):
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    
+       fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
 
-admin.site.register(Question)
+
+admin.site.register(Question, QuestionAdmin)
+
